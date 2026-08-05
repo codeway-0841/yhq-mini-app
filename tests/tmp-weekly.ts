66febd7cb136f9fbefe28d497bffe9d8e33ea68e
Ruslan Ablyamitov <codeway-0841@users.noreply.github.com>
2026-08-05 18:00:09 +0500

    chore: vaqtinchalik fayl o'chirildi

--- tests/tmp-weekly.ts
+++
@@ -1,2 +0,0 @@
-import { leaderboardRepository } from '../server/modules/leaderboard/leaderboard.repository'
-leaderboardRepository.weeklyTop(5, null).then(r => { console.log('OK', r.entries.length, r.weekStart); process.exit(0) }).catch(e => { console.error('ERR:', e.message); process.exit(1) })
